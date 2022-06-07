b48265416796214f8f6843ba9114c7b1d8827f98
akicitron308 <impeeeeedance@icloud.com>
2022-06-08 05:58:12 +0900

    fix: Added non-null constraint to table definition of 'mybeans' table
    There was room for null in the 'beans_id' and 'weight' columns.

--- app/models/mybeans.py
+++ app/models/mybeans.py
@@ -9,12 +9,12 @@ class Mybeans(Base):
     __tablename__ = "mybeans"
 
     id = Column(Integer, primary_key=True)
-    beans_id = Column(Integer, ForeignKey("beans.id"))
-    weight = Column(Float)
+    beans_id = Column(Integer, ForeignKey("beans.id"), nullable=False)
+    weight = Column(Float, nullable=False)
     roast_level = Column(String(30))
     roasted_date = Column(Date)
     grind_size = Column(String(30))
     grinded_date = Column(Date)
-    got_date = Column(Date)
+    got_date = Column(Date, nullable=False)
     created_date = Column(TIMESTAMP, nullable=False, server_default=text('current_timestamp'))
     updated_date = Column(TIMESTAMP, nullable=False, server_default=text('current_timestamp on update current_timestamp'))
